7386da0c337b381324940208f3a778be4b8829aa
Kevin McGillivray <kevin.mcgillivray@me.com>
2014-11-22 01:07:38 -0600

    Iterate panel.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,26 @@ var main = function() {
   $('.icon-panel').click(function() {
     var height = $('.top-panel').height();
     $('body').animate({
-        top: height
-    }, 300, 'linear');
+      top: height
+    }, 750, 'easeOutCirc');
+    
+    $('.navbar').animate({
+      backgroundColor: 'rgba(68, 68, 68, 0);'
+    }, 300);
+    $('.icon-panel').hide();
+    $('.icon-close').show();
   });
   
   $('.icon-close').click(function() {
-      $('body').animate({
-          top: '0px'
-      }, 300);
+    $('body').animate({
+      top: '0px'
+    }, 300);
+    
+    $('.navbar').animate({
+      backgroundColor: 'rgba(68, 68, 68, .75);'
+    }, 300);
+    $('.icon-panel').show();
+    $('.icon-close').hide();
   });
 };
 
