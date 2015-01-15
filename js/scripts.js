ee1e37ccf6e7c3d3181855449b6382d71ea14f88
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-01-14 22:42:15 -0600

    Fix top panel scrolling thing, Add angel post content.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 var main = function() {
+  
+  // Move the top panel out of view the same distance as its height.
   $(document).ready(function() {
     var height = $('.top-panel').height();
     $('.top-panel').css("top", -height);
   })
+  
+  // Show the panel, move the body down if scrolled to the top, update the navbar.
   $('.icon-panel').click(function() {
-    var height = $('.top-panel').height();
-    $('body').animate({
-      top: height
+        
+    $('.top-panel').animate({
+      top: '0px'
     }, 750, 'easeOutCirc');
+    
+    if ($(window).scrollTop() == 0) {
+      var height = $('.top-panel').height();
+      
+      $('body').animate({
+        top: height
+      }, 750, 'easeOutCirc');
+    }
     
     $('.navbar').animate({
       backgroundColor: 'rgba(23, 165, 204, 0);'
     }, 300);
+    
     $('.icon-panel').hide();
     $('.icon-close').show();
   });
   
+  // Hide the top panel, move the body back into normal position, reset the navbar.
   $('.icon-close').click(function() {
+    var height = $('.top-panel').height();
+    $('.top-panel').animate({
+      top: -height
+    }, 300);
+    
     $('body').animate({
       top: '0px'
     }, 300);
