bca8affc85f21befefd692d2e699f34d74bb085f
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-03-17 00:06:35 -0500

    Crazy javascript url project thing.

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,35 @@ var main = function() {
   });
 };
 
+$(document).on('click', '.popup-link', function (e) {
+    e.preventDefault();
+    var path = $(this).attr('data-link');
+    state = {action: 'popup'};
+    // Change URL in browser
+    history.pushState(state, '', path);
+});
+
+// Restore URL when popup is closed
+$(document).on("hidden.bs.modal", function (e) {
+    var currentstate = history.state;
+    if (currentstate) {
+        history.back();
+    }
+});
+
+// Listen for history state changes
+window.addEventListener('popstate', function(e) {
+    var state = history.state;
+    // back button pressed. close popup
+    if (!state) {
+        $('.modal.in').modal('hide');
+    }
+    else {
+        // Forward button pressed, reopen popup
+        var pathname = window.location.pathname;
+        var modal = $('body').find("[data-pathname='" + pathname + "']");
+        $(modal).modal('show');
+    }
+});
+
 $(document).ready(main);
